8cef48cd1bd90f76920daf9464b94f49a504fd7d
Neel Smith <dnsmith.neel@gmail.com>
2023-11-03 16:36:59 -0400

    Test using

--- lda-dashboard.jl
+++ lda-dashboard.jl
@@ -10,7 +10,16 @@ using CitableText
 using CitableCorpus
 using Orthography
 
+using CitableCorpusAnalysis
+
+using TextAnalysis
 using StatsBase, OrderedCollections
+
+# Widgetize these:
+iters = 1000
+stopwords = ["the", "and", "of"]
+
+
 
 assets = joinpath(pwd(), "dashboard", "assets")
 app = dash(assets_folder = assets, include_assets_files=true)
@@ -22,8 +31,9 @@ apollodorus_url = "https://raw.githubusercontent.com/neelsmith/digitalmyth/main/
 c =  fromcex(apollodorus_url, CitableTextCorpus, UrlReader)
 lextokens = filter(t -> t.tokentype == LexicalToken(), Orthography.tokenize(c, simpleAscii()))
 counts = countmap(map( t -> t.passage.text, lextokens)) |> OrderedDict
-
-
+sorteddict = sort(counts, rev=true, byvalue = true)
+k = 30
+lda_tm(c, k; stopwords = stopwords, iters = iters)#, doclabels = labels)
 app.layout = html_div() do
     html_div(className = "w3-container w3-light-gray w3-cell w3-mobile w3-border-left w3-border-right w3-border-gray",
     children = [dcc_markdown("*Dashboard version*: **$(DASHBOARD_VERSION)**")]
@@ -32,7 +42,7 @@ app.layout = html_div() do
     html_h1("Topic modeling dashboard"),
     html_div(className="w3-container",
     children = [
-        dcc_markdown("Did very much counting.")
+        dcc_markdown("Did very much counting. Distinct toknes: $(length(counts))")
     ])
     
 end
